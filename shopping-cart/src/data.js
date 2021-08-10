4e5cbf9d4194776e5d9e768982df1311648c0e9f
Igor <odaryukigor@gmail.com>
2021-08-10 16:27:54 +0300

    adding files

---
+++ shopping-cart/src/data.js
@@ -0,0 +1,26 @@
+const data = {
+    products: [
+        {
+            id: '1',
+            name: 'Chevrolet Corvette',
+            price: 55000,
+            image: 'https://images.unsplash.com/photo-1492144534655-ae79c964c9d7?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxzZWFyY2h8MXx8Y2Fyc3xlbnwwfHwwfHw%3D&auto=format&fit=crop&w=500&q=60',
+        },
+
+        {
+            id: '2',
+            name: 'Jaguar',
+            price: 67000,
+            image: 'https://images.unsplash.com/photo-1503376780353-7e6692767b70?ixid=MnwxMjA3fDB8MHxzZWFyY2h8Mnx8Y2Fyc3xlbnwwfHwwfHw%3D&ixlib=rb-1.2.1&auto=format&fit=crop&w=500&q=60',
+        },
+
+        {
+            id: '3',
+            name: 'Ferrari',
+            price: 95000,
+            image: 'https://images.unsplash.com/photo-1583121274602-3e2820c69888?ixid=MnwxMjA3fDB8MHxzZWFyY2h8NHx8Y2Fyc3xlbnwwfHwwfHw%3D&ixlib=rb-1.2.1&auto=format&fit=crop&w=500&q=60',
+        },
+    ],
+};
+
+export default data;
